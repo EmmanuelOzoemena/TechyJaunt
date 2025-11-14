09d731d90226b31382fd6b2d78026eca7120f344
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-11-14 20:56:29 +0100

    completed first assignment

---
+++ Assignment/1st/intro-to-backend-js-variable-datatype.js
@@ -0,0 +1,49 @@
+// 1. Declare a variable using let and assign your name to it.
+let firstName = "Emmanuel"
+let lastName = "Ozoemena"
+
+// Write JavaScript code to add two numbers and log the result.
+let num1 = 5;
+let num2 = 10; 
+let sum = num1 + num2;
+console.log(`Total: ${sum}`);
+
+// Create an object called student with properties: name, age, and department.
+const student = {
+  name: "Emmanuel Ozoemena",
+  age: 21,
+  department: "Computer Science"
+}
+
+// Write a JavaScript function called greet() that prints "Hello World".
+function greet() {
+  console.log("Hello World"); 
+}
+greet()
+
+// Write a program that checks if a number is even or odd.
+let number = 7;
+
+if (number % 2 === 0) {
+  console.log(`${number} is even`);
+} else {
+  console.log(`${number} is odd`);
+}
+
+// Create an array of 5 colors and print the first color.
+const colors = ["Red", "Blue", "Green", "Yellow", "Purple"];
+console.log(`First color: ${colors[0]}`);
+
+// Write a function that returns the square of a number passed into it.
+function square(num) {
+  return num * num
+}
+square(2)
+
+// Write a small code snippet that converts a string to a number:
+
+let value = "42";
+// convert to number here
+
+value = Number(value);
+console.log(`Converted number: ${value}`);
