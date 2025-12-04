bbf7d8ed11ee72276969ff22fc39b98d75bfa0fe
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-12-04 09:04:05 +0100

    created a logger middleware

--- practiceApp/routes/users.js
+++ practiceApp/routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
+router.use(logger);
+
 router.get("/", (req, res) => {
   res.send("User List");
 });
@@ -16,7 +18,7 @@ router.post("/", (req, res) => {
 router
   .route("/:id")
   .get((req, res) => {
-    console.log(req.user);
+    // console.log(req.user);
     res.send(`Get User with ID ${req.params.id}`);
   })
   .put((req, res) => {
@@ -27,10 +29,14 @@ router
   });
 
 const users = [{ name: "Ebuka" }, { name: "Ozoemena" }];
-
 router.param("id", (req, res, next, id) => {
   req.user = users[id];
   next();
 });
+
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 
 module.exports = router;
--- practiceApp/server.js
+++ practiceApp/server.js
@@ -3,16 +3,18 @@ const app = express();
 const port = 4000;
 
 app.set("view engine", "ejs");
+// app.use(logger);
 
 app.get("/", (req, res) => {
   res.render("index", { text: "World!" });
 });
 
+const userRouter = require("./routes/users");
+
+app.use("/users", userRouter);
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
-
-const userRouter = require("./routes/users");
-
-app.use("/users", userRouter);
