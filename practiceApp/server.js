cc95fa5591b1cce2ff5663ee7417501abfc946eb
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-12-04 09:35:06 +0100

    Practiced static middleware

--- practiceApp/server.js
+++ practiceApp/server.js
@@ -2,17 +2,18 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+app.use(express.static("public"));
+
 app.set("view engine", "ejs");
 // app.use(logger);
 
-app.get("/", (req, res) => {
-  res.render("index", { text: "World!" });
-});
+// app.get("/", (req, res) => {
+//   res.render("index", { text: "World!" });
+// });
 
 const userRouter = require("./routes/users");
 
 app.use("/users", userRouter);
-
 
 // Start the server
 app.listen(port, () => {
