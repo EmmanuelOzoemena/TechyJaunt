0eaf6b66e57792f8dbe15714b447f9819ea206b2
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-12-07 21:52:58 +0100

    Created registration schema

---
+++ Assignment/loanApp/models/loan.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const RegistrationSchema = mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: [true, "Please enter your first name"],
+    },
+
+    lastName: {
+      type: String,
+      required: [true, "Please enter your last name"],
+    },
+
+    email: {
+      type: String,
+      required: [true, "Please enter your email address"],
+    },
+
+    gender: {
+      type: String,
+      required: false,
+    },
+
+    dob: {
+      type: String,
+      required: false,
+    },
+
+    phoneNumber: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+const RegDetails = mongoose.model("RegDetails", RegistrationSchema);
+
+module.exports = RegDetails;
