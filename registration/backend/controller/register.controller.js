e4f3dcd095a5eaa97a9dae5d21160a413e69e28b
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2025-12-09 17:17:47 +0100

    added update and delete functions

--- registration/backend/controller/register.controller.js
+++ registration/backend/controller/register.controller.js
@@ -34,8 +34,46 @@ const uniqueDetail = async (req, res) => {
   }
 };
 
+// Update some details
+const updateDetail = async (req, res) => {
+  try {
+    const updateData = await RegDetails.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!updateData) {
+      return res.status(404).json({ message: "Record not found" });
+    }
+
+    res.status(200).json(updateData);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Delete details
+const deleteDetail = async (req, res) => {
+  try {
+    const deleteData = await RegDetails.findByIdAndDelete(req.params.id);
+
+    if (!deleteData) {
+      return res.status(404).json({ message: "Record not found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "Details deleted successfully", deleteData });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
 module.exports = {
   register,
   allDetails,
   uniqueDetail,
+  updateDetail,
+  deleteDetail,
 };
--- registration/backend/routes/register.routes.js
+++ registration/backend/routes/register.routes.js
@@ -6,10 +6,14 @@ const {
   register,
   allDetails,
   uniqueDetail,
+  updateDetail,
+  deleteDetail
 } = require("../controller/register.controller");
 
 router.post("/", register);
 router.get("/reg-details", allDetails);
 router.get("/reg-details/:id", uniqueDetail);
+router.put("/:id", updateDetail);
+router.delete("/:id", deleteDetail)
 
 module.exports = router;
