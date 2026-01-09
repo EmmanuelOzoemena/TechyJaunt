77afcdb3a0b9906612548dd775b0864e98a0cb11
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2026-01-09 01:04:37 +0100

    updated mongoDB connection

--- authentication/index.js
+++ authentication/index.js
@@ -3,9 +3,10 @@ const cors = require("cors");
 const app = express();
 const mongoose = require("mongoose");
 const morgan = require("morgan");
+const connectDB = require("./src/config/db");
 require("dotenv").config();
 
-const userRoutes = require("./src/routes/user.routes")
+const userRoutes = require("./src/routes/user.routes");
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,24 +14,16 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
+connectDB();
+
 // Home route
 app.get("/", (req, res) => {
   res.send("I dey aktive!");
 });
 
-
 app.get("/api/user", userRoutes);
 
-// MongoDB connection
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("Connected to the database");
-    // Start server
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch(() => {
-    console.log("Connection Failed");
-  });
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
