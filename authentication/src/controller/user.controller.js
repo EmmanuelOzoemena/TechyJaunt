d4584b650ef78668a287d9d9e8527082d1b8a302
Emmanuel Ozoemena <ozoemenachukwuebuka2004@gmail.com>
2026-01-12 15:10:55 +0100

    feat: implement login routes for user authentication

--- authentication/src/controller/user.controller.js
+++ authentication/src/controller/user.controller.js
@@ -6,6 +6,7 @@ const signup = async (req, res) => {
   const { name, email, password } = req.body;
 
   try {
+    // Checking if all fields are filled
     if (!name || !email || !password) {
       return res.status(400).json({ message: "All fields are required" });
     }
@@ -29,18 +30,40 @@ const signup = async (req, res) => {
 
     return res.status(201).json({ message: "User created successfully" });
   } catch (error) {
-    console.error("Error during signup:", error)
+    console.error("Error during signup:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
 
-// const login = async (req, res) => {
-//   console.log("Hello");
-// };
+const login = async (req, res) => {
+  const { email, password } = req.body;
 
+  try {
+    // Checking if all fields are filled
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
 
+    // Checking if the user exist in the database
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Checking if the user's password is correct
+    const comparePassword = await bcrypt.compare(password, user.password);
+    if (!comparePassword) {
+      return res.status(401).json({ message: "Invalid Credentials" });
+    }
+
+    return res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 module.exports = {
   signup,
-  // login,
+  login,
 };
